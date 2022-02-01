d91178088518dcbbe3b4b392a95fc846792d2b3a
debora-garcia <debora.garcia@g.globo>
2022-02-01 16:25:52 -0300

    alteracoes no app

--- TesteTeste/ViewController.swift
+++ TesteTeste/ViewController.swift
@@ -12,30 +12,19 @@ class ViewController: UIViewController {
 
     @IBAction func login(_ sender: Any) {
         print("Entrou na func de login")
-                
         
-        //self.present(UIViewController(), animated: true, completion: nil)
         self.present(ViewControllerScreen(), animated: true, completion: nil)
-
-//        self.dismiss(animated: true, completion: nil)
-//        self.present(ViewControllerScreen(), animated: true, completion: nil)
-//        present(ViewControllerScreen(), animated: true, completion: nil)
-        
-//        present(LoginViewControllerScreen(), animated: true, completion: nil)
-        print("Teste func")
     }
-    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
         let aux = BigBrotherBrasil(initials: "BBB", edition: 22)
         let aux2 = aux.getInitialsAndEdition()
         print(aux2)
     }
-
-
+    
+    
 }
 
